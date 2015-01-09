a81ab70a876f3fffea4331b469e1a5a59f6514eb
Laurence Berry <berry.laur@gmail.com>
2015-01-09 12:42:11 +0000

    add admin role and header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,16 @@ module ApplicationHelper
              :role  => "body" }
   end
 
+  def professions_index_page
+    controller.controller_name == "professions" && controller.action_name == "index"
+  end
+
+  def profession_show_page
+    controller.controller_name == "professions" && controller.action_name == "show"
+  end
+
+  def role_show_page
+    controller.controller_name == "roles" && controller.action_name == "show"
+  end
+
 end
