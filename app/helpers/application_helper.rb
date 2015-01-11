dc939e9a0e3fa396ea85b3159001092002139427
benrmatthews <ben@montfort.io>
2015-01-11 10:38:52 +0000

    Updated tweet button defaults

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   include TweetButton
   
-  TweetButton.default_tweet_button_options = {:via => "whocares", :count => "horizontal", :text => "Check out this role on @whocaresapp, your guide to who does what in the #NHS:"}
+  TweetButton.default_tweet_button_options = {:via => "whocaresapp", :count => "horizontal", :text => "Check out this role on @whocaresapp, your guide to who does what in the #NHS:"}
   
   def body_tags
     klasses = ''
