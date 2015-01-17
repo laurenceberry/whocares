2bc7124f6c3ba4f641f908a7c6c70628be02ffce
benrmatthews <ben@montfort.io>
2015-01-17 11:21:10 +0000

    Added sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -4,6 +4,9 @@ require 'sitemap_generator'
 SitemapGenerator::Sitemap.default_host = 'http://whocares.io'
 SitemapGenerator::Sitemap.create do
   add '/', :changefreq => 'daily', :priority => 0.9
-  add '/about', :changefreq => 'weekly'
+  add '/professions', :changefreq => 'daily', :priority => 0.8
+  add '/tags', :changefreq => 'daily', :priority => 0.8
+  add '/conditions', :changefreq => 'daily', :priority => 0.8
+  add '/about', :changefreq => 'weekly', :priority => 0.7
 end
 SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
