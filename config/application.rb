bb662dca75336db231be35314d3a28f242a770a6
benrmatthews <ben@montfort.io>
2015-01-17 10:40:20 +0000

    Added segment.io

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 require 'csv'
 require 'rails/all'
+require 'segment/analytics'
+
+Analytics = Segment::Analytics.new({
+    write_key: 'jjqlg13k3k',
+    on_error: Proc.new { |status, msg| print msg }
+})
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
