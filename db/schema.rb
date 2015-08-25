02658fdac0f22c280d6c6dfe1dda895b4358116d
benrmatthews <ben@montfort.io>
2015-08-25 02:00:07 +0000

    Improved search results page

--- db/schema.rb
+++ db/schema.rb
@@ -72,11 +72,6 @@ ActiveRecord::Schema.define(version: 20150823200058) do
     t.datetime "created_at"
   end
 
-  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
-  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
-  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
-  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
-
   create_table "jobs", force: true do |t|
     t.string   "title"
     t.text     "description"
